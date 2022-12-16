6d290b84553069fd30e903b0a0f01a03b0ca211e
Theo Budiyanto <theodorusib@gmail.com>
2022-12-16 18:26:01 +0700

    acimalakaing together

--- index.js
+++ index.js
@@ -3,12 +3,46 @@ const app = express();
 const port = 3000;
 const path = require("path");
 
+var expressWs = require("express-ws")(app);
+
+const connected = new Set();
+
+const start = (connected, ws, name) => {};
+
+app.ws("/connect", (ws, req) => {
+  ws.on("message", (msg) => {
+    const { name } = JSON.parse(msg);
+    if (name) {
+      connected.add(name);
+      console.log(`${name} is connected`);
+
+      setInterval(() => {
+        ws.send(
+          `<div id="pong">hi ${name}. there are currently ${connected.size - 1} other kodok acimalakaing with you</div>`
+        );
+      }, 1000);
+
+      ws.on("close", () => {
+        connected.delete(name);
+      });
+    }
+  });
+});
+
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.get("/connect", (req, res) => {
+  res.sendFile(path.join(__dirname, "connect.html"));
 });
+
+app.listen(port, () => {
+  console.log(`Acimalaka listening on port ${port}`);
+});
+
+setInterval(() => {
+  console.log("There are currently " + connected.size + " kodok acimalakaing with you.");
+}, 5000);
